a9a422d4329167219745db6476a9044ec5c1bb64
Elena <katsiashvili.elena@gmail.com>
2020-10-06 16:03:57 +0300

    Add JS_Lesson_08

---
+++ events.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function Task_1_1() {
+	text.classList.add('visibility');
+}
+
+function Task_1_2() {
+	hideButton.classList.add('visibility');
+}
+
+function Task_2() {
+	Visibility = document.getElementById('list').classList.toggle('visibility');
+}
